467a4871a240c5537ce740dfa0c898f79bce2863
raymond.d.abargos <raymond.d.abargos@accenturecom>
2018-06-14 10:00:09 +0800

    chore: added blurred filter

--- notebooks/3-instagram-filters.py
+++ notebooks/3-instagram-filters.py
@@ -15,9 +15,23 @@ import random
 def apply_invert(frame):
 	return cv2.bitwise_not(frame)
 
+def apply_alpha_convert(frame):
+    try:
+        frame.shape[3]
+    except IndexError:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        
+    return frame
+
+def apply_blend(frame1, frame2, mask):
+    alpha = mask / 255.0
+    blended = cv2.convertScaleAbs(frame1 * (1-alpha) + (frame2 * alpha))
+    
+    return blended
+
 def apply_sepia(frame, intensity=0.5):
     blue, green, red = 20, 66, 112
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     sepia_bgra = (blue, green, red, 1)
     
@@ -30,7 +44,7 @@ def apply_sepia(frame, intensity=0.5):
 
 def apply_red(frame, intensity=0.5):
     blue, green, red = 0, 0, 255
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     sepia_bgra = (blue, green, red, 1)
     
@@ -43,7 +57,7 @@ def apply_red(frame, intensity=0.5):
 
 def apply_blue(frame, intensity=0.5):
     blue, green, red = 255, 0, 0
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     sepia_bgra = (blue, green, red, 1)
     
@@ -56,7 +70,7 @@ def apply_blue(frame, intensity=0.5):
 
 def apply_green(frame, intensity=0.5):
     blue, green, red = 0, 255, 0
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     sepia_bgra = (blue, green, red, 1)
     
@@ -69,7 +83,7 @@ def apply_green(frame, intensity=0.5):
 
 def apply_disco(frame, intensity=0.5):
     blue, green, red = random.randint(0,255), random.randint(0,255), random.randint(0,255)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     sepia_bgra = (blue, green, red, 1)
     
@@ -80,7 +94,18 @@ def apply_disco(frame, intensity=0.5):
     
     return frame
 
-
+def apply_portrait_mode(frame):
+    frame = apply_alpha_convert(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+    
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    blurred = cv2.GaussianBlur(frame, (21, 21), 0)
+    
+    blended = apply_blend(frame, blurred, mask)
+    frame = cv2.cvtColor(blended, cv2.COLOR_BGRA2BGR)
+    
+    return frame
 
 cap = cv2.VideoCapture(0)
 
@@ -93,14 +118,16 @@ while True:
     blue = apply_blue(frame)
     green = apply_green(frame)
     disco = apply_disco(frame)
+    portrait = apply_portrait_mode(frame)
     
-    cv2.imshow('Frame', frame)
-    cv2.imshow('Inverted Frame', invert)
-    cv2.imshow('Sepia Frame', sepia)
-    cv2.imshow('Red Frame', red)
-    cv2.imshow('Blue Frame', blue)
-    cv2.imshow('Green Frame', green)
-    cv2.imshow('Disco Frame', disco)
+#    cv2.imshow('Frame', frame)
+#    cv2.imshow('Inverted Frame', invert)
+#    cv2.imshow('Sepia Frame', sepia)
+#    cv2.imshow('Red Frame', red)
+#    cv2.imshow('Blue Frame', blue)
+#    cv2.imshow('Green Frame', green)
+#    cv2.imshow('Disco Frame', disco)
+    cv2.imshow('Portrait', portrait)
     
     k = cv2.waitKey(1)
     
